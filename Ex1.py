f780dac05fb151e6cf1b9e273c7d40a3966384de
itamarKraitman <73484628+itamarKraitman@users.noreply.github.com>
2021-11-17 22:27:16 +0200

    fix bugs in buildings and Ex1

--- Ex1.py
+++ Ex1.py
@@ -25,6 +25,8 @@ def init():
 
 
 def allocate(f_loc3: str, calls: list, elevators: list, up_total: int, down_total: int):
+    print("up ",up_total, "down ",down_total)
+    ele_list = []  # list of elevators
     if len(elevators) == 1:
         for call in calls:
             call.who = 0
@@ -32,40 +34,36 @@ def allocate(f_loc3: str, calls: list, elevators: list, up_total: int, down_tota
     elif len(elevators) % 2 == 0:  # in case number of elevators is even
         for call in calls:
             if call.src > call.dest:  # up call
-                elevators = []
-            for i in range(0, int(len(elevators) / 2) + 1):
-                elevators.append(elevators[i])
-                call.who = find_closest(call=call, elevators=elevators)
+                for i in range(0, int(len(elevators) / 2)):
+                    ele_list.append(elevators[i])
+                call.who = find_closest(call=call, elevators=ele_list)
             else:
-                elevators = []
-                for i in range(int(len(elevators) / 2), int(len(elevators) + 1)):
-                    elevators.append(elevators[i])
-                call.who = find_closest(call=call, elevators=elevators)
+                for i in range(int(len(elevators) / 2), int(len(elevators))):
+                    ele_list.append(elevators[i])
+                call.who = find_closest(call=call, elevators=ele_list)
     else:  # in case number of elevators is odd
         if up_total > down_total:  # more up calls than down calls
             for call in calls:
                 if call.src > call.dest:  # up call
-                    elevators = []
-                for i in range(0, math.ceil(int(len(elevators) / 2) + 1)):
-                    elevators.append(elevators[i])
-                    call.who = find_closest(call=call, elevators=elevators)
+                    for i in range(0, math.ceil(int(len(elevators) / 2) + 1)):
+                        elevators.append(elevators[i])
+                    call.who = find_closest(call=call, elevators=ele_list)
                 else:  # down call
                     elevators = []
                     for i in range(math.floor(int(len(elevators) / 2)), int(len(elevators) + 1)):
                         elevators.append(elevators[i])
-                    call.who = find_closest(call=call, elevators=elevators)
+                    call.who = find_closest(call=call, elevators=ele_list)
         else:  # more down calls then up calls
             for call in calls:
                 if call.src > call.dest:  # up call
-                    elevators = []
-                for i in range(0, math.ceil(int(len(elevators) / 2) + 1)):
-                    elevators.append(elevators[i])
-                    call.who = find_closest(call=call, elevators=elevators)
-                else:  # down call
-                    elevators = []
-                    for i in range(math.floor(int(len(elevators) / 2)), int(len(elevators) + 1)):
+                    for i in range(0, math.ceil(int(len(elevators) / 2) + 1)):
                         elevators.append(elevators[i])
-                    call.who = find_closest(call=call, elevators=elevators)
+                        call.who = find_closest(call=call, elevators=ele_list)
+                    else:  # down call
+                        elevators = []
+                        for i in range(math.floor(int(len(elevators) / 2)), int(len(elevators) + 1)):
+                            elevators.append(elevators[i])
+                        call.who = find_closest(call=call, elevators=ele_list)
     write_csv(f_loc3, calls)
 
 
@@ -77,9 +75,9 @@ def find_closest(call: Call_Class.Call, elevators: list) -> int:
     for elevator in elevators:
         if elevator.currPos == call.src:
             return elevator.id  # in case elevator is in source position
-        elif abs(elevator.currPos - call.src) < abs(closest.currPos - call.src) | \
-                elevator.time_a_to_b(elevator.currPos, call.src) < closest.time_a_to_b(closest.currPos, call.src):
-            closest = elevator.id
+        elif (abs(elevator.currPos - call.src) < abs(closest.currPos - call.src)) | \
+                (elevator.time_a_to_b(elevator.currPos, call.src) < closest.time_a_to_b(closest.currPos, call.src)):
+            closest = elevator
     return closest.id
 
 
